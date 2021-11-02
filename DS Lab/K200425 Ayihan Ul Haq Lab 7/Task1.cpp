6402f7dd2b98b85726ec98179c21f58ecfdeffbe
Ayihan ul haq <ayihanhaq@gmail.com>
2021-11-02 20:36:08 +0500

    Lab 7

--- DS Lab/K200425 Ayihan Ul Haq Lab 7/Task1.cpp
+++ DS Lab/K200425 Ayihan Ul Haq Lab 7/Task1.cpp
@@ -28,12 +28,56 @@ class stack
 			{
 				a[++top]=x;
 				cout<<x<<"Pushed into stack";
+				return true;
 			}
+		}
+
+		int pop()
+		{
+			if(top<0)
+			{
+				cout<<"stack Underflow";
+			}
+
+			else
+			{
+				int x=a[top--];
+				return x;
+			}
+		}
+
+		int peek()
+		{
+			if(top<0)
+			{
+				cout<<"Stack is empty";
+				return 0;
+			}
+			else
+			{
+				int x=a[top];
+				return x;
+			}
+		}
+
+		bool isempty()
+		{
+			return (top<0);
 		}
 };
 
 int main(int argc, char const *argv[])
 {
+	stack stack;
+	int arr[10];
+
+	for(int i=0;i<10;i++)
+	{
+		cout<<"enter the element in the "<<i+1<<" position of the stack";
+		cin>>arr[i];
+		stack.push(arr[i]);
+	}
+
 	
 	return 0;
 }
