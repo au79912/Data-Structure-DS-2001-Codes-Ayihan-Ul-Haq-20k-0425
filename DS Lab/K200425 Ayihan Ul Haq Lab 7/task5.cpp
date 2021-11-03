84e054cc96568198c69f715efcc77b4b042417a0
Ayihan ul haq <ayihanhaq@gmail.com>
2021-11-03 06:43:03 +0500

    lab 7

--- DS Lab/K200425 Ayihan Ul Haq Lab 7/task5.cpp
+++ DS Lab/K200425 Ayihan Ul Haq Lab 7/task5.cpp
@@ -2,6 +2,30 @@
 
 using namespace std;
 
+struct node {
+   int data;
+   struct node *next;};
+struct node* front = NULL;
+struct node* rear = NULL;
+struct node* temp;
+void Insert() {
+   int val;
+   cout<<"Insert the element in queue : "<<endl;
+   cin>>val;
+   if (rear == NULL) {
+      rear = (struct node *)malloc(sizeof(struct node));
+      rear->next = NULL;
+      rear->data = val;
+      front = rear;
+   } else {
+      temp=(edlist struct node *);malloc(sizeof(struct node));
+      rear->next = temp;
+      temp->data = val;
+      temp->next = NULL;
+   rear = temp;
+   }}
+
+   //Sample Code has errors which makes the task hard to understand 
 
 
 int main(int argc, char const *argv[])
