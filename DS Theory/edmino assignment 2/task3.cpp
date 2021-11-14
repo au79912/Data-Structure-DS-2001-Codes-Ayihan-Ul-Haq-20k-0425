761393f5bad57deecde97c4c799716ec255e15ee
Ayihan ul haq <ayihanhaq@gmail.com>
2021-11-14 06:24:37 +0500

    It's about drive, it's about power We stay hungry, we devour Put in the work, put in the hours and take what's ours

--- DS Theory/edmino assignment 2/task3.cpp
+++ DS Theory/edmino assignment 2/task3.cpp
@@ -2,8 +2,108 @@
 
 using namespace std;
 
+class node
+{
+	public:
+		node *next;
+		node *prev;
+		string ID;
+		int time;
+		int quota;
+
+		node()
+		{
+			next = NULL;
+			prev = NULL;
+			ID = "\0";
+			time = 0;
+			quota = 0;
+		}
+		node(string id, int t, int q)
+		{
+			next = NULL;
+			prev = NULL;
+			ID = id;
+			time = t;
+			quota = q;
+		}
+};
+
+class linkedList
+{
+	node *head;
+	node *tail;
+	node *temp;
+
+	public:
+		linkedList()
+		{
+			head = NULL;
+			tail = NULL;
+		}
+
+		void insert(string id, int t, int q)
+		{
+			node *temp = new node(id, t, q);
+			if (head == NULL)
+			{
+				head = temp;
+				tail = temp;
+			}
+			else
+			{
+				tail->next = temp;
+				temp->prev = tail;
+				tail = temp;
+			}
+		}
+
+		void check_print()
+		{
+			temp = head;
+			int n = temp->time;
+			int m = temp->quota;
+			while(head->time != 0)
+			{
+				if(temp->time != 0)
+				{
+					cout<<temp->ID<<endl;
+					temp->time -= temp->quota;
+				}
+				temp = temp->next;
+	
+				if(temp->time != 0)
+				{
+					cout<<temp->ID<<endl;
+					temp->time -= temp->quota;
+				}
+				temp = temp->next;
+
+				if(temp->time != 0)
+				{
+					cout<<temp->ID<<endl;
+					temp->time -= temp->quota;
+				}
+				n-=m;
+				temp=head;
+			}
+		}
+};
+
 int main(int argc, char const *argv[])
 {
+	linkedList l;
+	int m;
 	
+	cin>>m;
+
+	for(int i=0;i<m; i++)
+	{
+		string id;
+		int t, q;
+		cin>>id>>t>>q;
+		l.insert(id, t, q);
+	}
+	l.check_print();
 	return 0;
 }
