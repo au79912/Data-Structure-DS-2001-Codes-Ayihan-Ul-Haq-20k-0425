885ae71c2c7d009bf1c7edd42ba63d4086fab950
Ayihan ul haq <ayihanhaq@gmail.com>
2021-12-01 15:06:47 +0500

    project

--- Project/project_task1.cpp
+++ Project/project_task1.cpp
@@ -288,7 +288,7 @@ int main(int argc, char const *argv[])
 	int k = rand() % max2-min2 +min2;
 	
 	//solver thread
-	thread solver1(puzzle_answer_1, k);
+	thread solver1(puzzle_answer_1, 45);
 	thread solver2(puzzle_answer_2,j);
 	thread solver3(puzzle_answer_3,"abcdefghijklmnopqrstuvwxyz");
 	
