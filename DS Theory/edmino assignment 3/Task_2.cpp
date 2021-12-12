35497059acd94d08b910b7681994aef137e25132
Ayihan ul haq <ayihanhaq@gmail.com>
2021-12-12 11:18:52 +0500

    task 2 -> task 3

--- DS Theory/edmino assignment 3/Task_2.cpp
+++ DS Theory/edmino assignment 3/Task_2.cpp
@@ -1,75 +1,145 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
 
-#include <iostream> 
-#include <vector> 
- 
-using namespace std; 
+using namespace std;
 
-struct Node
+//node class for bst
+class Node
 {
-	int data; 
-	struct Node *left, *right; 
-}; 
-  
-// return height of tree 
-int heightoftree(Node* root) 
-{
-
-	if (root == NULL) 
-		return 0; 
-
-	int left = heightoftree(root->left); 
-	int right = heightoftree(root->right); 
-
-	return ((left > right ? left : right) + 1); 
-}
-
-void printPerLevelMinimum(Node* root,vector<int>& res, int level) 
-{
-	if (root != NULL) 
-	{ 
-
-		printPerLevelMinimum(root->left, res, level + 1); 
-
-		if (root->data < res[level]) 
-			res[level] = root->data; 
-
-		printPerLevelMinimum(root->right,res, level + 1); 
+public:
+	int data;
+	Node* left;
+	Node* right;
+	Node(int data)
+	{
+		this->data = data;
+		left = NULL;
+		right = NULL;
 	}
-}
+};
 
-void perLevelMinimumUtility(Node* root) 
+//BST class with level wise traversal
+class BST
 {
-	int n = heightoftree(root), i; 
-	vector<int> res(n, INT_MAX); 
-	printPerLevelMinimum(root, res, 0); 
+public:
+	Node* root;
+	BST()
+	{
+		root = NULL;
+	}
+	void insert(int data)
+	{
+		Node* newNode = new Node(data);
+		if (root == NULL)
+		{
+			root = newNode;
+		}
+		else
+		{
+			Node* temp = root;
+			while (temp != NULL)
+			{
+				if (data < temp->data)
+				{
+					if (temp->left == NULL)
+					{
+						temp->left = newNode;
+						break;
+					}
+					else
+					{
+						temp = temp->left;
+					}
+				}
+				else
+				{
+					if (temp->right == NULL)
+					{
+						temp->right = newNode;
+						break;
+					}
+					else
+					{
+						temp = temp->right;
+					}
+				}
+			}
+		}
+	}
+	void levelOrder()
+	{
+		if (root == NULL)
+		{
+			cout << "Tree is empty" << endl;
+		}
+		else
+		{
+			queue<Node*> q;
+			q.push(root);
+			while (!q.empty())
+			{
+				Node* temp = q.front();
+				q.pop();
+				cout << temp->data << " ";
+				if (temp->left != NULL)
+				{
+					q.push(temp->left);
+				}
+				if (temp->right != NULL)
+				{
+					q.push(temp->right);
+				}
+			}
+		}
+	}
+	void print_level_order()
+	{
+		if (root == NULL)
+		{
+			cout << "Tree is empty" << endl;
+		}
+		else
+		{
+			queue<Node*> q;
+			q.push(root);
+			while (!q.empty())
+			{
+				Node* temp = q.front();
+				q.pop();
+				cout << temp->data << " ";
+				if (temp->left != NULL)
+				{
+					q.push(temp->left);
+				}
+				if (temp->right != NULL)
+				{
+					q.push(temp->right);
+				}
+			}
+		}
+	}
+};
 
-	cout << "Every level minimum is"; 
-	for (i = 0; i < n; i++) 
-	{ 
-		cout << "level " << i <<" min is = "<< res[i] << " "; 
-	} 
+//driver function
+int main()
+{
+	BST bst;
+	int arr[10];
+	
+	for (int i=0;i<10;i++)
+	{
+		cin>>arr[i];
+	}
+
+	for (int i=0;i<10;i++)
+	{
+		bst.insert(arr[i]);
+	}
+
+	bst.levelOrder();
+	cout << endl;
+	bst.print_level_order();
+	return 0;
 }
-
-Node* newNode(int data) 
-{ 
-	Node* temp = new Node; 
-	temp->data = data; 
-	temp->left = temp->right = NULL; 
-
-	return temp; 
-} 
-  
-int main() 
-{ 
-	Node* root = newNode(7); 
-	root->left = newNode(6); 
-	root->right = newNode(5); 
-	root->left->left = newNode(4); 
-	root->left->right = newNode(3); 
-	root->right->left = newNode(2); 
-	root->right->right = newNode(1); 
-
-	perLevelMinimumUtility(root); 
-
-	return 0; 
-} 
